cb54d357eb5208fbcbc0c80f0df2b7f73999b6ae
ChipButt <jameschipbutt@hotmail.com>
2026-06-09 06:14:03 +0100

    Restore data loader and fix venue asset paths

--- data.js
+++ data.js
@@ -1,1 +1,4 @@
-__PLACEHOLDER__
+(function(){
+  var src="https://cdn.jsdelivr.net/gh/ChipButt/TRR@4c6946f2e45b0e8ff6d2f4d672a9e0d6a6c31638/data.js";
+  document.write('<script src="'+src+'"><\/script>');
+  document.write('<script>(function(){var d=window.RESTORATION_ROUTE_DATA;if(!d||!d.assets)return;for(var i=1;i<=8;i++){d.assets["venue"+i]="assets/8_venue_assets_venue_"+i+"_ui.webp";}d.tabAssetOverrides=d.tabAssetOverrides||{};for(var j=1;j<=8;j++){d.tabAssetOverrides["assets/venue_"+j+"_ui.jpg"]="assets/8_venue
